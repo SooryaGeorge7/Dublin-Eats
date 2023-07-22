b5025e6501e86a38b1c26a8a21ee59b15743728c
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-22 21:38:03 +0000

    Feat:Updated views.signup, base.html

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserSignupForm
+
+
 
 def signup(request):
-    
-        form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Signup Succesful {username}!You May Now Log In!')
+            form.save()
+            return redirect('dublineats-home')
+    else:
+        form = UserSignupForm()
     return render(request, 'users/signup.html', {'form': form})
 
