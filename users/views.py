c59cfb7c595e9330e774a735fb7c31fcd94de801
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-22 21:01:54 +0000

    Feat: Added views.signup in users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
+def signup(request):
+    
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
 # Create your views here.
