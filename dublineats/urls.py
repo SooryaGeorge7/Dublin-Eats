25498bbccf3f9d405b8f5999b652f9909e4bb662
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-22 22:04:50 +0000

    Feat:Added urls for login/logout

--- dublineats/urls.py
+++ dublineats/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', user_views.signup, name='signup'),
+    path('userlogin/', auth_views.LoginView.as_view(template_name='users/login.html'), name='userlogin'),
+    path('userlogout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'), name='userlogout'),
     path('', include('home.urls')),
 ]
