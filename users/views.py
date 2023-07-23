3f1f2810b7b31d29f6487657ab83b3b36de77d57
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-23 21:41:04 +0000

    Feat:Added views.edit_profile

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserSignupForm
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import UserSignupForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserSignupForm, UserUpdateForm, ProfileUpdateForm
 
 
 def signup(request):
@@ -34,3 +34,31 @@ def profile(request):
 
     return render(request, "users/profile.html", context)
 
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(
+                request, f'Your account has been updated!')
+        
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'profile': profile,
+        'user':user,
+    }
+
+    return render(request, 'users/edit_profile.html', context)
