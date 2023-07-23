a9e94ab98eb026a105fb23339df228d588c5251b
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-23 09:17:49 +0000

    Feat:Added profile view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserSignupForm
-
+from django.contrib.auth.models import User
+from .models import Profile
+from .forms import UserSignupForm
 
 
 def signup(request):
@@ -16,4 +18,19 @@ def signup(request):
     else:
         form = UserSignupForm()
     return render(request, 'users/signup.html', {'form': form})
+
+
+def profile(request):
+    """
+    Renders the users profile, checks that the user matches profile user
+    """
+    # user = get_object_or_404(User, username=username)
+
+    profile = Profile.objects.get(user=request.user)
+
+    context = {
+        "profile": profile,
+    }
+
+    return render(request, "users/profile.html", context)
 
