f8e88d4eac02bac78cfc4f34006faff03e6c3d8d
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-23 08:54:13 +0000

    Feat:Added profile model in users.models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+placeholder= (f"https://res.cloudinary.com/dif9bjzee/image/upload/c_scale,h_50,w_50/v1689590568/profile-gda8c660e4_640_z54qoi.webp")
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(
+        max_length=50, null=True, blank=True, default="Firstname"
+    )
+    surname = models.CharField(
+        max_length=50, null=True, blank=True, default="Surname"
+    )
+    about = models.TextField(null=True,
+        blank=True,
+        max_length=200,
+        default="Foodie in and out..!",)
+    
+    profile_image = CloudinaryField(
+        "image",
+        default = placeholder,
+        eager=[{"width": 50, "height": 50, "crop": "crop"}],
+        transformation={
+            "width": 50,
+            "height": 50,
+            "crop": "fill",
+        },
+        blank= True,
+        null= True,
+    )
+    fav_food = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        default="What's your favourite cuizine?",
+    )
+    
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
