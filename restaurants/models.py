f75f7be486370a8f121c4eafa896f2c18419d38b
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-27 16:29:37 +0000

    Feat:Added Restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+    address = models.TextField()
+    RestaurantId = models.CharField(max_length=50, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
