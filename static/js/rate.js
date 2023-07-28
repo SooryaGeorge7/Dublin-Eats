cab6d202c223cdb66c08c6aaf2430e6e75754bef
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-28 14:08:34 +0000

    Feat: Implemented js functionality to review form

---
+++ static/js/rate.js
@@ -0,0 +1,68 @@
+var form = document.querySelector('.reviewform');
+
+var handleRatings = (choice, criteria) => {
+    var stars = document.querySelectorAll(`.${criteria} .btn i`);
+
+    stars.forEach(star => star.classList.remove('checked'));
+
+    switch (choice) {
+        case 'first':
+            for (var i = 0; i < 1; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'second':
+            for (var i = 0; i < 2; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'third':
+            for (var i = 0; i < 3; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'fourth':
+            for (var i = 0; i < 4; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'fifth':
+            for (var i = 0; i < 5; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+    }
+};
+
+var getNumValue = (stringValue) => {
+    let numValue;
+    if (stringValue === 'first') {
+        numValue = 1;
+    } else if (stringValue === 'second') {
+        numValue = 2;
+    } else if (stringValue === 'third') {
+        numValue = 3;
+    } else if (stringValue === 'fourth') {
+        numValue = 4;
+    } else if (stringValue === 'fifth') {
+        numValue = 5;
+    } else {
+        numValue = 0;
+    }
+    return numValue;
+};
+
+var criterias = ['taste', 'ambience', 'customer_service', 'location', 'value_for_money'];
+
+criterias.forEach(criteria => {
+    var stars = document.querySelectorAll(`.${criteria} .btn i`);
+
+    stars.forEach(star => {
+        star.addEventListener('click', (event) => {
+            handleRatings(event.target.id, criteria);
+            var val = event.target.id;
+            var val_num = getNumValue(val);
+            form[criteria].value = val_num;
+        });
+    });
+});
