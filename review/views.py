70a3b9a3d6d8a3991c836fa5dfe1e14f9c65617c
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-07-30 09:38:23 +0000

    Fix:Updated review.views,py

--- review/views.py
+++ review/views.py
@@ -5,7 +5,7 @@ from .models import Review
 from restaurants.models import Restaurant
 from django.contrib.auth.models import User
 
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
