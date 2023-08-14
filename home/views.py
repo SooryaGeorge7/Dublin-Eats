c992c6f17a59a5b34cd4424d96a43923614fe23c
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-08-14 13:41:02 +0000

    Fix:Imported API

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-
+GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
 
 def search(request):
     category = request.GET.get("query")
