805beeb2ecca4d32e7c398c809e05541f7c67be2
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-08-14 13:36:52 +0000

    Feat:Added searchresult view

--- home/views.py
+++ home/views.py
@@ -16,10 +16,102 @@ def search(request):
         return redirect("searchresults", category=category)
     print(category)
 
+def get_details(place_id):
+    detail_url = "https://maps.googleapis.com/maps/api/place/details/json"
+    params = {
+        "place_id": place_id,
+        "fields": "website,photos",
+        "key": GOOGLE_PLACES_API_KEY,
+    }
+    response = requests.get(detail_url, params=params)
+    detail_data = response.json()
+    return detail_data.get("result", {})
 
 def searchresults(request, category):
+    url = ( 
+        f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={category}&type=restaurant&location=53.350140,-6.266155&key={GOOGLE_PLACES_API_KEY}"
+    )
+    user = request.user
+    restaurant_results = []
+    response = requests.get(url)
+    restaurant_data = response.json()
+    results = restaurant_data.get("results", [])
+    # print(results)    
+    paginator = Paginator(results, 8)  
+    page_number = request.GET.get("page")
+    page_object = paginator.get_page(page_number)
+    
+    
+    for result in page_object:
+            
+            place_id = result.get("place_id")
+            if place_id:
+                detail_data = get_details(place_id)
+                website_url = detail_data.get("website", "")
+                photos = detail_data.get("photos", "")
+                image_urls = []
+                if photos:
+                    for photo in photos:
+                        photo_reference = photo.get('photo_reference')
+                        if photo_reference:
+                            image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={GOOGLE_PLACES_API_KEY}"
+                            image_urls.append(image_url)
+                else:
+                    image_urls.append("https://res.cloudinary.com/dif9bjzee/image/upload/v1688163762/backgroud_hwsqzo.webp")
+            
+            
+            
+            try:
+                restaurant_details = Restaurant.objects.get(RestaurantId=place_id)
+            except Restaurant.DoesNotExist:
+                restaurant_details = Restaurant(
+                    name = result["name"],
+                    website = website_url,
+                    category = category,
+                    address = result["formatted_address"],
+                    RestaurantId = place_id,
+                )            
+                print(restaurant_details)
+                restaurant_details.save()
+            
+            # user_review = Review.objects.filter(restaurant=restaurant_details, user=user)
+            
+            # if user_review.exists():
+            #     user_reviewed = True
+            # else:
+            #     user_reviewed = False
+            pinned = False
+            user_reviewed = False  # Initialize the user_reviewed variable to False
+            if request.user.is_authenticated:
+                  # If the user is signed in, check if they have reviewed the restaurant
+                user_review = Review.objects.filter(restaurant=restaurant_details, user=user)
+                profile = Profile.objects.get(user=user)
+                if user_review.exists():
+                    user_reviewed = True
+                if profile.pinned_restaurants.filter(RestaurantId=place_id).exists():
+                    pinned = True
 
-    return 
+            restaurant_results.append({
+                "name": result["name"],
+                "category": category,
+                "address": result["formatted_address"],
+                "image_urls" : image_urls,
+                "pinned": pinned,
+                "user_reviewed" : user_reviewed,
+                "website_url": website_url,
+                "place_id": place_id,
+                
+            })
+
+            
+    return render(request, 'home/results.html', {
+        "restaurant_results":restaurant_results,
+        "page_object": page_object,
+        "restaurant_details": restaurant_details,
+        })
+
+
+   
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
