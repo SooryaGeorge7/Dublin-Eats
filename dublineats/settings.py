7eb0cd6828f15e1f4a8540be39047480e9806698
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-08-24 09:56:01 +0000

    deployment commit

--- dublineats/settings.py
+++ dublineats/settings.py
@@ -29,7 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['dublineats-f1f59ebc3584.herokuapp.com','8000-sooryageorge-dublineats-aulnazm1c9r.ws-eu102.gitpod.io',
 '8000-sooryageorge-dublineats-aulnazm1c9r.ws-eu103.gitpod.io','8000-sooryageorge-dublineats-aulnazm1c9r.ws-eu104.gitpod.io','localhost']
