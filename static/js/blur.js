c7bc7357915c746dd33c78c2822f68bc4950acb6
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-08-24 16:10:31 +0000

    Fix:Handle Onfocus with eventlistener

--- static/js/blur.js
+++ static/js/blur.js
@@ -1,8 +1,21 @@
-function onFocus() {
-    document.querySelector('.hero-image').classList.add('blury');
-  }
+// function onFocus() {
+//     document.querySelector('.hero-image').classList.add('blury');
+//   }
   
-function onFocusOut() {
-    document.querySelector('.hero-image').classList.remove('blury');
-  }
+// function onFocusOut() {
+//     document.querySelector('.hero-image').classList.remove('blury');
+//   }
 
+const searchfield = document.getElementById('query');
+
+const heroImage = document.querySelector('.hero-image');
+  
+
+searchfield.addEventListener('focus', function() {
+    heroImage.classList.add('blury');
+});
+  
+
+searchfield.addEventListener('focusout', function() {
+    heroImage.classList.remove('blury');
+});
