530654eec41d3514e8ce5b5c025c43be3332679d
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-08-27 11:42:57 +0000

    Refractor:Validated home.views code

--- home/views.py
+++ home/views.py
@@ -11,8 +11,6 @@ from django.core.paginator import Paginator
 GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
 
 
-
-
 def get_details(place_id):
     detail_url = "https://maps.googleapis.com/maps/api/place/details/json"
     params = {
@@ -24,6 +22,7 @@ def get_details(place_id):
     detail_data = response.json()
     return detail_data.get("result", {})
 
+
 def search(request):
     """
     Takes the users query from the search bar and makes a call to the TMDB API
@@ -34,96 +33,99 @@ def search(request):
     if query:
         return redirect("searchresults", query=query)
 
+
 def searchresults(request, query):
 
     # query = request.GET.get("query")
-    url = ( 
-        f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&type=restaurant&location=53.350140,-6.266155&key={GOOGLE_PLACES_API_KEY}"
+    url = (
+        f"https://maps.googleapis.com/maps/api/place/textsearch/json?"
+        f"query={query}&type=restaurant&location=53.350140,-6.266155"
+        f"&key={GOOGLE_PLACES_API_KEY}"
     )
     user = request.user
     restaurant_results = []
     response = requests.get(url)
     restaurant_data = response.json()
     results = restaurant_data.get("results", [])
-    # print(results)    
-    paginator = Paginator(results[:10], 8)  
+    # print(results)
+    paginator = Paginator(results[:10], 8)
     page_number = request.GET.get("page")
     page_object = paginator.get_page(page_number)
-    
-    
+
     for result in page_object:
-            
-            place_id = result.get("place_id")
-            if place_id:
-                detail_data = get_details(place_id)
-                website_url = detail_data.get("website", "")
-                photos = detail_data.get("photos", "")
-                image_urls = []
-                if photos:
-                    for photo in photos:
-                        photo_reference = photo.get('photo_reference')
-                        if photo_reference:
-                            image_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={GOOGLE_PLACES_API_KEY}"
-                            image_urls.append(image_url)
-                else:
-                    image_urls.append("https://res.cloudinary.com/dif9bjzee/image/upload/v1692544795/default-image_kyuezj.webp")
-            
-            
-            
-            try:
-                restaurant_details = Restaurant.objects.get(RestaurantId=place_id)
-                restaurant_details.category = query
-                restaurant_details.save()
-            except Restaurant.DoesNotExist:
-                restaurant_details = Restaurant(
-                    name = result["name"],
-                    website = website_url,
-                    category = query,
-                    address = result["formatted_address"],
-                    RestaurantId = place_id,
-                )            
-                print(restaurant_details)
-                restaurant_details.save()
-            
-            # user_review = Review.objects.filter(restaurant=restaurant_details, user=user)
-            
+        place_id = result.get("place_id")
+        if place_id:
+            detail_data = get_details(place_id)
+            website_url = detail_data.get("website", "")
+            photos = detail_data.get("photos", "")
+            image_urls = []
+            if photos:
+                for photo in photos:
+                    photo_reference = photo.get('photo_reference')
+                    if photo_reference:
+                        image_url = (
+                            f"https://maps.googleapis.com/maps/api/place/"
+                            f"photo?maxwidth=400&photoreference="
+                            f"{photo_reference}&key={GOOGLE_PLACES_API_KEY}"
+                        )
+                        image_urls.append(image_url)
+            else:
+                image_urls.append("https://res.cloudinary.com/dif9bjzee/"
+                                  f"image/upload/v1692544795/"
+                                  f"default-image_kyuezj.webp")
+
+        try:
+            restaurant_details = Restaurant.objects.get(RestaurantId=place_id)
+            restaurant_details.category = query
+            restaurant_details.save()
+        except Restaurant.DoesNotExist:
+            restaurant_details = Restaurant(
+                name=result["name"],
+                website=website_url,
+                category=query,
+                address=result["formatted_address"],
+                RestaurantId=place_id,
+            )
+            print(restaurant_details)
+            restaurant_details.save()
+
             # if user_review.exists():
             #     user_reviewed = True
             # else:
             #     user_reviewed = False
-            pinned = False
-            user_reviewed = False  # Initialize the user_reviewed variable to False
-            if request.user.is_authenticated:
-                  # If the user is signed in, check if they have reviewed the restaurant
-                user_review = Review.objects.filter(restaurant=restaurant_details, user=user)
-                profile = Profile.objects.get(user=user)
-                if user_review.exists():
-                    user_reviewed = True
-                if profile.pinned_restaurants.filter(RestaurantId=place_id).exists():
-                    pinned = True
+        pinned = False
+        user_reviewed = False  # Initialize the user_reviewed variable to False
+        if request.user.is_authenticated:
+            user_review = Review.objects.filter(restaurant=restaurant_details,
+                                                user=user)
+            profile = Profile.objects.get(user=user)
+            if user_review.exists():
+                user_reviewed = True
+            if (
+                profile.pinned_restaurants
+                .filter(RestaurantId=place_id)
+                .exists()
+            ):
+                pinned = True
 
-            restaurant_results.append({
-                "name": result["name"],
-                
-                "address": result["formatted_address"],
-                "image_urls" : image_urls,
-                "pinned": pinned,
-                "user_reviewed" : user_reviewed,
-                "website_url": website_url,
-                "place_id": place_id,
-                
-            })
+        restaurant_results.append({
+            "name": result["name"],
+            "address": result["formatted_address"],
+            "image_urls": image_urls,
+            "pinned": pinned,
+            "user_reviewed": user_reviewed,
+            "website_url": website_url,
+            "place_id": place_id,
+        })
 
-            
     return render(request, 'home/results.html', {
-        "restaurant_results":restaurant_results,
+        "restaurant_results": restaurant_results,
         "page_object": page_object,
         "restaurant_details": restaurant_details,
         "query": query,
         })
 
 
-   
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
